fa2314e7363375e78becd1403836db9e4211973b
anoninkriz <15214494+antoninkriz@users.noreply.github.com>
2020-04-23 16:42:20 +0200

    Updated dont.js - added blur event detection, added new execution methods

--- dont.js
+++ dont.js
@@ -1,11 +1,23 @@
-var s = document.createElement('script')
-s.textContent =
-  '(function() {' +
-  'var a = Node.prototype.addEventListener;' +
-  'Node.prototype.addEventListener = function(e) {' +
-  "if (e !== 'visibilitychange' && e !== 'webkitvisibilitychange') {" +
-  'a.apply(this, arguments)' +
-  '}}' +
-  '})()'
-;(document.head || document.documentElement).appendChild(s)
-s.remove()
+function _DONT_() {
+    for (let t of ["visibilitychange", "webkitvisibilitychange", "blur"]) {
+        window.addEventListener(t, function(e) {
+            e.stopImmediatePropagation();
+        }, true);
+    }
+
+    let x = window.Node.prototype.addEventListener;
+    window.Node.prototype.addEventListener = function(e) {
+        if (e !== 'visibilitychange' && e !== 'webkitvisibilitychange' && e !== 'blur') {
+            x.apply(this, arguments)
+        }
+    }
+}
+
+// v1
+_DONT_();
+
+// v2
+const script = document.createElement('script');
+script.textContent = `${_DONT_.toString()};_DONT_();`;
+(document.head || document.documentElement).appendChild(script);
+script.remove();
